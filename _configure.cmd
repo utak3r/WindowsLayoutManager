c89349eee451ac182d294ed74dd7456cd410e2b2
utak3r <piotr.borys@gmail.com>
2024-08-14 12:12:01 +0200

    Init repository

---
+++ _configure.cmd
@@ -0,0 +1,10 @@
+set VS_PATH=C:\Program Files\Microsoft Visual Studio\2022\Enterprise
+set CMAKE_PATH=C:\devel\CMake\bin
+@call _set_user_paths.cmd
+set PATH=%CMAKE_PATH%;%PATH%
+
+@call "%VS_PATH%\Common7\Tools\vsdevcmd.bat" -no_ext -winsdk=none %*
+
+mkdir build-mvs22
+cd build-mvs22
+cmake -G "Visual Studio 17 2022" ..
