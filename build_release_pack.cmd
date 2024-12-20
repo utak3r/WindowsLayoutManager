11f06ece8b44e7be28609f6b5edf28f1f238d9aa
utak3r <piotr.borys@gmail.com>
2024-12-20 15:38:39 +0100

    CI stuff, packing and installing stuff mainly.

--- build_release_pack.cmd
+++
@@ -1,8 +0,0 @@
-set VS_PATH=C:\Program Files\Microsoft Visual Studio\2022\Enterprise
-set CMAKE_PATH=C:\devel\CMake\bin
-set PATH=%CMAKE_PATH%;%PATH%
-
-@call "%VS_PATH%\Common7\Tools\vsdevcmd.bat" -no_ext -winsdk=none
-cd build-release
-cmake --build . --config Release
-cpack --config CPackConfig.cmake
